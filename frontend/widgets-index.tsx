a8ae0b963cbb81f47ae78176783af4aa9f4f96bc
Peter Lemiszki <plemiszki@gmail.com>
2024-10-26 15:59:43 -0400

    static index page

--- frontend/widgets-index.tsx
+++ frontend/widgets-index.tsx
@@ -1,13 +1,52 @@
 import React from "react";
-import { Table, TableContainer, Typography } from "@mui/material";
+import {
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+interface Widget {
+  name: string;
+}
+
+const WIDGETS: Widget[] = [
+  {
+    name: "Widget 1",
+  },
+  {
+    name: "Widget 2",
+  },
+];
 
 function WidgetsIndex() {
   return (
     <>
-      <Typography>Widgets</Typography>
-      <TableContainer>
-        <Table></Table>
-      </TableContainer>
+      <Stack sx={{ m: 2 }} spacing={2}>
+        <Typography>Widgets</Typography>
+        <Paper sx={{ width: "100%" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {WIDGETS.map(({ name }: Widget) => {
+                return (
+                  <TableRow>
+                    <TableCell>{name}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+      </Stack>
     </>
   );
 }
