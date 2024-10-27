75960bb2bec71145e57ed45e3a7b79234684e747
Peter Lemiszki <plemiszki@gmail.com>
2024-10-27 10:11:56 -0400

    details wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "application#root"
-  get "/moo" => "application#root"
+  get "/details" => "application#root"
   namespace :api do
     resources :widgets, only: [ :index, :create, :show, :update, :destroy ]
   end
--- frontend/entry.tsx
+++ frontend/entry.tsx
@@ -1,27 +1,19 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WidgetsIndex from "./widgets-index";
+import WidgetDetails from "./widget-details";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <WidgetsIndex />,
   },
+  {
+    path: "/details",
+    element: <WidgetDetails />,
+  },
 ]);
-
-function App() {
-  const hello: string = "heyooooo";
-  const n: number = 2;
-  return (
-    <div>
-      {hello} {n}
-      <Link to={`moo`}>Moo Page</Link>
-      <Button variant="contained">Hello world</Button>
-    </div>
-  );
-}
 
 const appDiv = document.getElementById("app");
 const root = createRoot(appDiv);
