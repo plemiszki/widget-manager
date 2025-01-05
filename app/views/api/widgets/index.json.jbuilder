c5281dacc4e0417250b527607b423272bad2f059
Peter Lemiszki <plemiszki@gmail.com>
2025-01-05 15:14:16 -0500

    procfile release command, widget age

--- app/views/api/widgets/index.json.jbuilder
+++ app/views/api/widgets/index.json.jbuilder
@@ -1,4 +1,5 @@
 json.widgets @widgets do |widget|
   json.id widget.id
   json.name widget.name
+  json.age widget.age
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_05_163834) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_05_200108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,5 +18,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_05_163834) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age", default: 0
   end
 end
