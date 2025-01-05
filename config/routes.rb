7b768a209e6ce3bc15ebe1b3ae4f4574ae6026d2
Peter Lemiszki <plemiszki@gmail.com>
2025-01-05 12:59:16 -0500

    api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "application#root"
+  get "/widgets/:id" => "application#root"
+  namespace :api do
+    resources :widgets, only: [ :index, :create, :show, :update, :destroy ]
+  end
 end
