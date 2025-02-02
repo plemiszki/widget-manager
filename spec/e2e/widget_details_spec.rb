5cb7d54370f57a4a196c97489660d53f9beafdc3
Peter Lemiszki <plemiszki@gmail.com>
2025-02-02 15:37:41 -0500

    test - delete widget

--- spec/e2e/widget_details_spec.rb
+++ spec/e2e/widget_details_spec.rb
@@ -11,4 +11,16 @@ describe 'widget_details', type: :feature do
     expect(age_field[:value]).to eq('22')
   end
 
+  it 'deletes the widget' do
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    delete_button = find('button', text: 'DELETE')
+    delete_button.click
+    confirm_button = find('button', text: 'YES')
+    confirm_button.click
+
+    expect(page).to have_current_path("/widgets")
+    expect(Widget.count).to eq(0)
+  end
+
 end
