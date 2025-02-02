466ceefbc9cab7b79420a812a71c54a5467acc21
Peter Lemiszki <plemiszki@gmail.com>
2025-02-02 16:21:10 -0500

    test - update widget validations

--- spec/e2e/widget_details_spec.rb
+++ spec/e2e/widget_details_spec.rb
@@ -27,6 +27,82 @@ describe 'widget_details', type: :feature do
     expect(widget.age).to eq(40)
   end
 
+  it 'validates the presence of the widget name' do
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    name_field = find_all('input')[0]
+
+    name_field.set(' ')
+    save_button = find('button', text: 'SAVE')
+    save_button.click
+
+    expect(page).to have_no_css("div[data-test='spinner']")
+    expect(page).to have_content("Name can't be blank")
+    widget.reload
+    expect(widget.name).to eq('Test Widget')
+  end
+
+  it 'validates the uniqueness of the widget name' do
+    Widget.create!(name: "Existing Name", age: 22)
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    name_field = find_all('input')[0]
+
+    name_field.set('Existing Name')
+    save_button = find('button', text: 'SAVE')
+    save_button.click
+
+    expect(page).to have_no_css("div[data-test='spinner']")
+    expect(page).to have_content("Name has already been taken")
+    widget.reload
+    expect(widget.name).to eq('Test Widget')
+  end
+
+  it 'validates the numericality of the widget age' do
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    name_field, age_field = find_all('input')
+
+    age_field.set('asdf')
+    save_button = find('button', text: 'SAVE')
+    save_button.click
+
+    expect(page).to have_no_css("div[data-test='spinner']")
+    expect(page).to have_content("Age is not a number")
+    widget.reload
+    expect(widget.age).to eq(22)
+  end
+
+  it 'validates the widget age is an integer' do
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    name_field, age_field = find_all('input')
+
+    age_field.set('3.4')
+    save_button = find('button', text: 'SAVE')
+    save_button.click
+
+    expect(page).to have_no_css("div[data-test='spinner']")
+    expect(page).to have_content("Age must be an integer")
+    widget.reload
+    expect(widget.age).to eq(22)
+  end
+
+  it 'validates the widget age is greater than zero' do
+    widget = Widget.create!(name: "Test Widget", age: 22)
+    visit widget_path(widget)
+    name_field, age_field = find_all('input')
+
+    age_field.set('-1')
+    save_button = find('button', text: 'SAVE')
+    save_button.click
+
+    expect(page).to have_no_css("div[data-test='spinner']")
+    expect(page).to have_content("Age must be greater than 0")
+    widget.reload
+    expect(widget.age).to eq(22)
+  end
+
   it 'deletes the widget' do
     widget = Widget.create!(name: "Test Widget", age: 22)
     visit widget_path(widget)
