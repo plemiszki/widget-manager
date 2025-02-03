05410024bcccb5c9882afb94c64dd3aa26123f10
Peter Lemiszki <plemiszki@gmail.com>
2025-02-02 19:13:39 -0500

    test - add widget

--- spec/e2e/widgets_index_spec.rb
+++ spec/e2e/widgets_index_spec.rb
@@ -9,4 +9,22 @@ describe 'widgets_index', type: :feature do
     expect(page).to have_content '22'
   end
 
+  it 'creates a new widget' do
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('name')
+    age_field.set('16')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    widget = Widget.first
+    expect(widget.name).to eq('name')
+    expect(widget.age).to eq(16)
+    expect(page).to have_content 'name'
+    expect(page).to have_content '16'
+  end
+
 end
