44d0cf4fe527e9ffaff08736b5adc7b6d3791c7b
Peter Lemiszki <plemiszki@gmail.com>
2025-02-02 19:30:50 -0500

    tests - add widget validations

--- spec/e2e/widgets_index_spec.rb
+++ spec/e2e/widgets_index_spec.rb
@@ -27,4 +27,80 @@ describe 'widgets_index', type: :feature do
     expect(page).to have_content '16'
   end
 
+  it 'validates the presence of the widget name' do
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('')
+    age_field.set('16')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    expect(page).to have_content("Name can't be blank")
+    expect(Widget.count).to eq(0)
+  end
+
+  it 'validates the uniqueness of the widget name' do
+    Widget.create!(name: 'name', age: 1)
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('name')
+    age_field.set('16')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    expect(page).to have_content("Name has already been taken")
+    expect(Widget.count).to eq(1)
+  end
+
+  it 'validates the numericality of the widget age' do
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('name')
+    age_field.set('age')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    expect(page).to have_content("Age is not a number")
+    expect(Widget.count).to eq(0)
+  end
+
+  it 'validates the widget age is an integer' do
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('name')
+    age_field.set('4.5')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    expect(page).to have_content("Age must be an integer")
+    expect(Widget.count).to eq(0)
+  end
+
+  it 'validates the widget age is greater than zero' do
+    visit widgets_path
+    add_button = find('button', text: "ADD WIDGET")
+    add_button.click
+
+    name_field, age_field = find_all('input')
+    name_field.set('name')
+    age_field.set('-1')
+    add_button = find('button', text: /^ADD$/)
+    add_button.click
+
+    expect(page).to have_content("Age must be greater than 0")
+    expect(Widget.count).to eq(0)
+  end
+
 end
