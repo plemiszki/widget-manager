f12a6f2baef8f66e7ef3f2552fde45b5a41a32aa
Peter Lemiszki <plemiszki@gmail.com>
2025-02-18 21:28:30 -0500

    let's try this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :null_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :solid_queue
