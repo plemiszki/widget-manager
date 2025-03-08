f2722a315301d8e1fe3c7bec819a0c6a664931b0
Peter Lemiszki <plemiszki@gmail.com>
2025-03-08 14:49:24 -0500

    spacing

--- spec/controllers/session_helper.rb
+++ spec/controllers/session_helper.rb
@@ -1,7 +1,9 @@
 module SessionHelper
+
   def create_user_session
     cookies.signed[:session_id] = Session.first.id
   end
+
 end
 
 RSpec.configure do |config|
--- spec/e2e/auth_helper.rb
+++ spec/e2e/auth_helper.rb
@@ -1,8 +1,10 @@
 module AuthenticationHelper
+
   def authenticated_path(path, user)
     visit "/test_login?user_id=#{user.id}"
     path
   end
+
 end
 
 RSpec.configure do |config|
