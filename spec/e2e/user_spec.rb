bea4011cffaf15cd0145518cacc63811923bee35
Peter Lemiszki <plemiszki@gmail.com>
2025-03-14 17:02:58 -0400

    sign up test - happy path

--- spec/e2e/user_spec.rb
+++ spec/e2e/user_spec.rb
@@ -28,4 +28,23 @@ describe 'user', type: :feature do
     expect(page).to have_button('Sign in')
   end
 
+  it 'signs up' do
+    visit new_user_path
+    expect(page).to have_content('Sign Up')
+    email_field, password_field = find_all('input')
+
+    email_field.set("user@domain.com")
+    password_field.set("password")
+
+    sign_up_button = find('button', text: 'CREATE ACCOUNT')
+    sign_up_button.click
+    expect(page).to have_no_css("div[data-test='spinner']")
+
+    expect(User.count).to eq(1)
+    user = User.first
+    expect(user.email_address).to eq("user@domain.com")
+
+    expect(page).to have_current_path('/session/new')
+  end
+
 end
