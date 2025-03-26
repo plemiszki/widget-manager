2cbe5fcd7d8ec2b32c848b24bbbc80ddbe3a6703
Peter Lemiszki <plemiszki@gmail.com>
2025-03-25 22:02:13 -0400

    404 page - style

--- app/javascript/components/not-found.tsx
+++ app/javascript/components/not-found.tsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Paper, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
 function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <Stack sx={{ p: 2 }} spacing={2}>
-      <Typography>Not Found</Typography>
-      <Stack direction="row">
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={() => navigate("/widgets")}
-        >
-          Home
-        </Button>
-      </Stack>
-    </Stack>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ pt: 4 }}
+    >
+      <Paper sx={{ p: 2, width: "100%", maxWidth: 500 }}>
+        <Stack spacing={2} display="flex" alignItems="center">
+          <QuestionMarkIcon sx={{ fontSize: 50 }} />
+          <Typography variant="h1">Page Not Found</Typography>
+          <Stack direction="row">
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => navigate("/widgets")}
+              sx={{
+                textTransform: "none",
+              }}
+            >
+              Home
+            </Button>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Box>
   );
 }
 
